a9c55a3b5e7df8f26a9996d70bde35fa076d85ee
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-01-31 16:03:29 -0700

    Complete javascript exercise 4 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, removeElement) {
+    for (let i = 0; i < arguments.length-1; i++) {
+        if (array.includes(arguments[i+1])) {
+            let index = array.indexOf(arguments[i+1]);
+            array.splice(index, 1);
+        }
+        else {
+            continue;
+        }
+    }
+    return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
