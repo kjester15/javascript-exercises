c6a656800c36d875bcb4a07f2c46302aea8e9757
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-01-31 16:31:37 -0700

    Complete javascript exercise 5 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let total = 0;
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
 
+    if (typeof num1 == 'number' && typeof num2 == "number") {
+        if (num1 > num2) {
+            for (let i = num2; i <= num1; i++) {
+                total += i;
+            }
+        }
+        else {
+            for (let i = num1; i <= num2; i++) {
+                total += i;
+            }
+        }
+    
+        return total;
+    }
+    return "ERROR";
+    
 };
+
+sumAll(123,1);
 
 // Do not edit below this line
 module.exports = sumAll;
