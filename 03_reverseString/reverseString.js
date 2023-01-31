834f5b503bdef32d1de01fb7e615330b3d63d941
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-01-31 15:44:10 -0700

    Complete javascript exercise 3 reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse = "";
+    let stringLen = string.length;
+    for (let i = 0; i < stringLen; i++) {
+        reverse += string[(stringLen-1-i)];
+    }
+    return reverse;
 };
 
 // Do not edit below this line
